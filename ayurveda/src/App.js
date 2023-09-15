c644fff424d792ff8a4714a1c730b58e93b6ca35
atharvasankdm <atharva19sk@gmail.com>
2023-09-15 22:40:13 +0530

    merging changes

--- ayurveda/src/App.js
+++ ayurveda/src/App.js
@@ -6,15 +6,21 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar1 from "./components/Navbar1";
 import Home1 from "./pages/Home1";
 import "bootstrap/dist/css/bootstrap.min.css";
+import VideoLearning from "./pages/VideoLearning";
+import Chatbot from "./components/Chatbot";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar1 />
+
         <Routes>
           <Route path="/" element={<Home1 />} />
+          <Route path="/videos" element={<VideoLearning />} />
         </Routes>
         {/* <Footer /> */}
+
+        <Chatbot />
       </BrowserRouter>
     </div>
   );
